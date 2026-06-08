27bc55419fe09642f69ddf3d3a5039674f9dfbfb
Avanthika S I <siavanthika030807@gmail.com>
2026-06-08 19:16:51 +0530

    Day-1 of practice

---
+++ day1.java
@@ -0,0 +1,23 @@
+class day1 {
+    public int[] twoSum(int[] nums, int target) {
+        int[] arr = new int[2];
+
+        int n = nums.length;
+
+        boolean flag = false;
+
+        for(int i=0; i<n-1; i++) {
+            for(int j=i+1; j<n; j++) {
+                if(nums[i] + nums[j] == target) {
+                    arr[0] = i;
+                    arr[1] = j;
+                    flag = true;
+                    break;
+                }
+            }
+            if(flag) break;
+        }
+
+        return arr;
+    }
+}
